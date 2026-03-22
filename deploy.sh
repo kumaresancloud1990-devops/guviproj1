595c65631e9884370a4e094ecb40f4ff66ec56a4
devopskum <kumardevops@gmail.com>
2026-03-22 19:38:51 +0000

    repo info added in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE=yourdockerhub/dev-react-app:latest
+IMAGE=kumaresancloud1990/dev-react-app:latest
 
 echo "Pulling latest image..."
 docker pull $IMAGE
@@ -10,4 +10,4 @@ docker stop react-app || true
 docker rm react-app || true
 
 echo "Running new container..."
-docker run -d -p 80:80 --name react-app $IMAGE
+docker run -d -p 8080:80 --name react-app $IMAGE
