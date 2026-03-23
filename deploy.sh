2aaa6721c54e32e53134f4b271e72c5ae85408db
devopskum <kumardevops@gmail.com>
2026-03-23 19:39:55 +0000

    Fixed deploy.sh shebang issue

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,12 @@
-# #!/bin/bash
-
-# IMAGE=kumaresancloud1990/dev-react-app:latest
-
-# echo "Pulling latest image..."
-# docker pull $IMAGE
-
-# echo "Stopping old container..."
-# docker stop react-app || true
-# docker rm react-app || true
-
-# echo "Running new container..."
-# docker run -d -p 8080:80 --name react-app $IMAGE
 #!/bin/bash
-
-!/bin/bash
 
 IMAGE_NAME=$1
 
 echo "Stopping old container..."
 docker rm -f react-app || true
 
+echo "Pulling latest image..."
+docker pull $IMAGE_NAME
+
 echo "Running new container..."
-docker run -d -p 8085:80 --name react-app $IMAGE_NAME
+docker run -d -p 80:80 --name react-app $IMAGE_NAME
